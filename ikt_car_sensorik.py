c2d6a6d976a48ebf5be0471a7b3ad9c7bffbd610
Rene1706 <rene.huertgen@gmx.de>
2023-06-17 13:35:04 +0200

    Completed Infrared and Encoder handling with parking space length calc

--- ikt_car_sensorik.py
+++ ikt_car_sensorik.py
@@ -4,6 +4,7 @@ from time import time, sleep
 import threading
 import RPi.GPIO as GPIO
 import smbus
+import math
 
 GPIO.setmode(GPIO.BCM)
 
@@ -138,18 +139,22 @@ class Infrared(object):
 
 	def __init__(self,address):
 		self.address = address
+		bus.write_byte(self.address, 0x00, 0x00)
 		
 	# Aufgabe 2 
 	#
 	# In dieser Methode soll der gemessene Spannungswert des Infrarotsensors ausgelesen werden.
 	def get_voltage(self):
-		return 0
+		adc_value = bus.read_byte_data(self.address, 0x00)
+		return adc_value/255.0 * 5.0
 
 	# Aufgabe 3
 	#
 	# Der Spannungswert soll in einen Distanzwert umgerechnet werden.
 	def get_distance(self):
-		return 0
+		voltage = self.get_voltage()
+		distance = voltage/5.0 * 70 + 10
+		return distance
 
 
 class InfraredThread(threading.Thread):
@@ -157,32 +162,53 @@ class InfraredThread(threading.Thread):
 
 	# distance to an obstacle in cm
 	distance = 0
-
 	# length of parking space in cm
 	parking_space_length = 0
-
+	dist_to_recognice_parking = 3
+	start_distance = 0
+	start_travel_distance = 0
+	end_travel_distance = 0
+	in_parking_space = False
 	# Aufgabe 4
 	#
 	# Hier muss der Thread initialisiert werden.
 	def __init__(self, address, encoder=None):
-		return 0
+		threading.Thread.__init__(self)
+		self.infrared = Infrared(address)
+		self.encoder = encoder
+		self.stopped = False
+		self.start_distance = self.infrared.get_distance()
+		self.start()
 
 	def run(self):
 		while not self.stopped:
-			read_infrared_value()
-			calculate_parking_space_length()
+			self.read_infrared_value()
+			self.calculate_parking_space_length()
 
 	# Aufgabe 4
 	#
 	# Diese Methode soll den Infrarotwert aktuell halten
 	def read_infrared_value(self):	
-		return 0
+		self.distance = self.infrared.get_distance()
 
 	# Aufgabe 5
 	#
 	# Hier soll die Berechnung der Laenge der Parkluecke definiert werden
 	def calculate_parking_space_length(self):
-		return 0
+		# Check if distance has increased more than 3 cm -> next to parking space
+		if self.start_distance > self.distance + self.dist_to_recognice_parking:
+			if self.in_parking_space == False:
+				self.in_parking_space = True
+				self.start_travel_distance = self.encoder.get_travel_dist()
+			elif self.in_parking_space == True:
+				# Update new parking space length as long as we are next to parking space
+				self.parking_space_length = self.encoder.get_travel_dist()-self.start_travel_distance
+		else:
+			if self.in_parking_space == True:
+				self.in_parking_space = False
+				self.end_travel_distance = self.encoder.get_travel_dist()
+				# Final parking space length after end of parking space
+				self.parking_space_length = self.end_travel_distance-self.start_travel_distance
 
 	def stop(self):
 		self.stopped = True
@@ -192,18 +218,27 @@ class InfraredThread(threading.Thread):
 #################################################################################
 	
 class Encoder(object):
-	''' This class is responsible for handling encoder data '''
+	''' This class is resNoneponsible for handling encoder data '''
 
 	# Aufgabe 2
 	#
 	# Wieviel cm betraegt ein einzelner Encoder-Schritt?
-	STEP_LENGTH = 0 # in cm
+	D = 3 # Durchmesser
+	N = 20 # Number of steps
+	STEP_LENGTH = math.pi*D/N # in cm
 
 	# number of encoder steps
 	count = 0
 
-	def __init__(self, pin):
+	def __init__(self, pin=3):
 		self.pin = pin
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.pin, GPIO.IN)
+		GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self.count, bouncetime = 1)
+
+
+	def count(self, channel):
+		self.count += 1
 
 	# Aufgabe 2
 	#
@@ -214,7 +249,8 @@ class Encoder(object):
 	# 
 	# Diese Methode soll die gesamte zurueckgelegte Distanz zurueckgeben.
 	def get_travelled_dist(self):
-		return 0
+		return self.count * self.STEP_LENGTH
+
 
 class EncoderThread(threading.Thread):
 	''' Thread-class for holding speed and distance data of all encoders'''
@@ -229,17 +265,36 @@ class EncoderThread(threading.Thread):
 	#
 	# Hier muss der Thread initialisiert werden.
 	def __init__(self, encoder):
-		return 0
+		threading.Thread.__init__(self)
+		self.encoder = encoder
+		self.prev_distance = 0
+		self.prev_time = time()
+		self.stopped = False
+		self.start()
 
 	def run(self):
 		while not self.stopped:
-			get_values()
+			self.get_values()
 
 	# Aufgabe 4
 	#
 	# Diese Methode soll die aktuelle Geschwindigkeit sowie die zurueckgelegte Distanz aktuell halten.
 	def get_values(self):
-		return 0
+		current_count = self.encoder.count
+		current_distance = self.encoder.get_travelled_dist()
+		current_time = time()
+
+		elapsed_time = current_time-self.prev_time
+		distance_delta = current_distance-self.prev_distance
+
+		if elapsed_time > 0:
+			self.speed = distance_delta/elapsed_time
+		else:
+			self.speed = 0
+
+		self.distance = current_distance
+		self.prev_time = current_time
+		self.prev_distance = current_distance
 
 
 	def stop(self):
